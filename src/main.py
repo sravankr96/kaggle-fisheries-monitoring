d0a963364c0481ee1a1cb9994908baa610481d92
Sravan Reddy <sravankr96@gmail.com>
2017-03-05 18:15:50 +0530

    Implemented localization task, localization_as_regr model

---
+++ src/main.py
@@ -0,0 +1,13 @@
+"""
+Copyright © Sr@1 2017, All rights reserved
+
+Contains the complete end to end training and prediction
+"""
+from tasks.localizer import run_localizer
+
+"""
+    * Running localizer task
+        * Archetecture of the localizer can be found at /model_archs/localizer_as_regr.py
+        * Settings for the localizer task can be found at /tasks/localizer.py
+"""
+run_localizer()
